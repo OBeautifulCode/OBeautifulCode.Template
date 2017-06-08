b3487146cac90b4f067073427e0eab668eae7f3c
Suraj K. Gupta <surajg@gmail.com>
2017-06-08 14:14:20 -0400

    seeding template

---
+++ OBeautifulCode.Template.Test/MyClassTest.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MyClassTest.cs" company="OBeautifulCode">
+//   Copyright (c) OBeautifulCode. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OBeautifulCode.Template.Test
+{
+    using Xunit;
+
+    public static class MyClassTest
+    {
+        [Fact]
+        public static void Method___Should_do_something___When_in_some_state()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/ad93171lk8re0s3o?svg=true)](https://ci.appveyor.com/project/SurajGupta/obeautifulcode-template)
+
+
+Project Name Here
+==================
+Project description here.
